7758a0bbf28473c65678c38fbedba473ec8f7cfd
Nikola Stanković <nikola@stankovic.xyz>
2016-05-10 21:00:34 +0200

    Finished the calculator module
    - Implemented the calculator
    - tested with the tast cases of the video in media folder

--- solution/module/calculator/scripts/calculator.js
+++ solution/module/calculator/scripts/calculator.js
@@ -1,25 +1,35 @@
 /**
  * core
  */
+var left = 0;
+var right = 0;
+var operator = "";
+
 function calculate(left, operator, right) {
-    var result = 0;
+    var result = "Invalid calculation";
 
     switch(operator) {
         case "+":
-            result = left + right;
+            result = parseInt(left) + parseInt(right);
             break;
         case "-":
-            result = left - right;
+            result = parseInt(left) - parseInt(right);
             break;
         case "*":
-            result = left * right;
+            result = parseInt(left) * parseInt(right);
             break;
         case "/":
-            result = left / right;
+            result = parseInt(left) / parseInt(right);
             break;
     }
 
     return result;
+}
+
+function clearVariables() {
+    left = "";
+    right = "";
+    operator = "";
 }
 
 /**
@@ -27,28 +37,52 @@ function calculate(left, operator, right) {
  */
 window.addEventListener('load', function() {
 
-    var left = 0;
-    var right = 0;
+    $(".output").val("Welcome");
 
     $(".number").on('click', function () {
+        if($(".output").val() == "Welcome") {
+            $(".output").val("");
+        }
+
         $("#input").append(this.value);
     });
 
     $(".operator").on('click', function () {
+        if(operator == "") {
+            operator = this.value;
             left = $("#input").val();
-        
-        
-            $("#output").append(left + " " + this.value);
-            $("#input").val("");
-    });
 
-    $("button").on('click', function () {
-        // var value = this.value;
-        // alert("Hi, Nikola :) The Value is: " + value);
+            $("#output").append(left + " " + operator);
+            $("#input").val("");
+        } else {
+            operator = this.value;
+
+            var content = $(".output").val();
+            var endSize = content.length -1;
+            var result = content.substring(0, endSize);
+
+            $(".output").val(result + operator);
+        }
     });
     
     $("#key-c").on('click', function () {
         $("#input").val("");
         $("#output").val("");
+        clearVariables();
+    })
+
+    $("#key-equals").on('click', function () {
+        if(left != "") {
+            right = $("#input").val();
+
+            var result = calculate(left, operator, right);
+
+            $("#input").val(result);
+            $("#output").val("");
+
+            clearVariables();
+        } else {
+            $("#output").val("Invalid Calculation");
+        }
     })
 });
