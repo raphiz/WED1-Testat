2bbc0d1e59e93718914ef522042cca8d067bd9ec
Raphael Zimmermann <mail@raphael.li>
2016-05-11 15:45:26 +0200

    Schissi CSS und HTML FUCK gmacht...FUCK bin ich hässig

--- solution/js/font-size.js
+++ solution/js/font-size.js
@@ -1,1 +1,6 @@
-
+document.getElementById('font-slider').addEventListener('change', function(event){
+    var value = event.target.value;
+    document.getElementById('font-size').value = value;
+    document.body.style.fontSize = value + "px";
+    console.log(document.body.style);
+});
