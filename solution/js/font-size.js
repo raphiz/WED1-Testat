257dba86189d5133ca098191be9eb8304e8f9bb3
Raphael Zimmermann <mail@raphael.li>
2016-05-13 10:23:26 +0200

    Ersetze font slider px mit em

--- solution/js/font-size.js
+++ solution/js/font-size.js
@@ -1,6 +1,5 @@
 document.getElementById('font-slider').addEventListener('change', function(event){
-    var value = event.target.value;
-    document.getElementById('font-size').value = value;
-    document.body.style.fontSize = value + "px";
-    console.log(document.body.style);
+    var value = event.target.value/15;
+    document.getElementById('font-size').value = Math.round(value * 10) / 10 ;
+    document.body.style.fontSize = value + "rem";
 });
